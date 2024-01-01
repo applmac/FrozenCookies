36db4686c93ac7dbc2658b5a4d06fd3554f29274
applmac <39588608+applmac@users.noreply.github.com>
2024-01-01 15:12:02 -0500

    Update fc_bookmarklet_loader.js

--- fc_bookmarklet_loader.js
+++ fc_bookmarklet_loader.js
@@ -1,1 +1,1 @@
-javascript:(function(){Game.LoadMod("https://github.erbkaiser.com/FrozenCookies/frozen_cookies.js");}());
+javascript:(function(){Game.LoadMod("https://github.com/applmac/FrozenCookies/blob/main/frozen_cookies.js");}());
